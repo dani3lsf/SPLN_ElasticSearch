0a1c28a528b52d0b0b69aeb0a269c1a80c4e2e67
Helena Poleri <a78633@alunos.uminho.pt>
2018-11-18 17:35:49 +0000

    loading sample data

--- elasticsearchSPLN.py
+++ elasticsearchSPLN.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
+#------------------------------------------------------------------------------
 
 import json
 from elasticsearch import Elasticsearch
 from datetime import datetime
 
-#import credentials
-credentialsJson = open('credentials.json','r')
-credentials = json.load(credentialsJson)
-credentialsJson.close()
+# Init ids for articles
+id_a = 1
 
-#Setup elasticsearch
+# Import credentials
+credentials_json = open('credentials.json', 'r')
+credentials = json.load(credentials_json)
+credentials_json.close()
+
+# Setup elasticsearch
 es = Elasticsearch(credentials['es_endpoint'], 
-    http_auth=(credentials['username'],credentials['password']))
+    http_auth = (credentials['username'], credentials['password']))
 
+# Create an index (ignore if it already exists)
+es.indices.create(index = 'news', ignore = 400)
 
-""" Exemplo de uso do elasticsearch """
+# 
+with open('data/data.jsonl') as f:
+	data = f.readlines()
 
-doc = {
-    'author': 'kimchy',
-    'text': 'Elasticsearch: cool. bonsai cool.',
-    'timestamp': datetime.now(),
-}         
+for line in data:
+    es.index(index = 'news', doc_type = 'test-type', id = id_a, body = line)
+    id_a += 1
+    
+res = es.get(index = 'news', doc_type = 'test-type', id = 42)
+print(res)
 
-res = es.index(index="test-index", doc_type='tweet', id=1, body=doc)
-print(res['result'])
+#es.indices.refresh(index="test-index")
 
-res = es.get(index="test-index", doc_type='tweet', id=1)
-print(res['_source'])
-
-es.indices.refresh(index="test-index")
-
-res = es.search(index="test-index", body={"query": {"match_all": {}}})
-print("Got %d Hits:" % res['hits']['total'])
-for hit in res['hits']['hits']:
-    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+#res = es.search(index="test-index", body={"query": {"match_all": {}}})
+#print("Got %d Hits:" % res['hits']['total'])
+#for hit in res['hits']['hits']:
+#    print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
  
-res = es.delete(index="test-index", doc_type='tweet', id=1)
-print(res['result'])
+#res = es.delete(index="test-index", doc_type='tweet', id=1)
+#print(res['result'])
 
